e9a7528fca337be80961cbc12932a3f1f15d4abc
krlvm <51774833+krlvm@users.noreply.github.com>
2021-12-20 19:34:06 +0300

    Add copyright headers

--- src/main/java/io/github/krlvm/powertunnel/plugins/adblock/AdBlock.java
+++ src/main/java/io/github/krlvm/powertunnel/plugins/adblock/AdBlock.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of LibertyTunnel.
+ * This file is part of PowerTunnel-AdBlock.
  *
- * LibertyTunnel is free software: you can redistribute it and/or modify
+ * PowerTunnel-AdBlock is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * LibertyTunnel is distributed in the hope that it will be useful,
+ * PowerTunnel-AdBlock is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PowerTunnel.  If not, see <https://www.gnu.org/licenses/>.
+ * along with PowerTunnel-AdBlock.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package io.github.krlvm.powertunnel.plugins.adblock;
--- src/main/java/io/github/krlvm/powertunnel/plugins/adblock/RequestListener.java
+++ src/main/java/io/github/krlvm/powertunnel/plugins/adblock/RequestListener.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of PowerTunnel-AdBlock.
+ *
+ * PowerTunnel-AdBlock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PowerTunnel-AdBlock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PowerTunnel-AdBlock.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.krlvm.powertunnel.plugins.adblock;
 
 import io.github.krlvm.powertunnel.sdk.http.ProxyRequest;
